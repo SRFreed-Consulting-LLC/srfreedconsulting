8820894399f295a29d52d20e8136232bb7951a09
Shane Freed <shane.freed@gmail.com>
2024-03-20 10:51:58 -0400

    added leadership

---
+++ src/app/pages/leadership/data.ts
@@ -0,0 +1,32 @@
+
+
+// Our roster has an opening with your name on it
+
+// We’re looking for passionate technologists, strong communicators, and those with a commitment to continuous improvement who own delivery as Software Engineering Manager, working for our Sportsbook Vertical.
+
+// As a Software Engineering Manager, you will have established Software Engineer capabilities. You’re excited to learn how to develop and grow Engineers and will start out with a small number of line-reports to put your learnings to the test. You’ll be responsible for providing an out-standing employee experience tailored to your line report’s individual needs. You’ll be responsible for leading projects or features within a team by applying your technical leadership and people management experience to support a team and drive for delivery excellence.
+
+// As a Software Engineering Manager, you’ll be working closely with our engineers day-to-day to support and mentor them as they tackle tough technical challenges. You’ll help find opportunities focused on providing the best employee experience while balancing delivery of capabilities that work at scale. You’ll own your software, helping define solutions to problems while mitigating long-term risks, own raising and helping mitigate blockers while providing status updates to leadership. The ideal candidate would be experienced in:
+
+// A strong technology background delivering software as an individual contributor or manager.
+// Communication, presentation, and facilitation skills
+// Strong sense of ownership, urgency, and drive
+// Demonstrated abilities driving operational excellence and best practices.
+// Demonstrated experience in shipping high quality products and services.
+// Proficiency in relevant technologies
+// Able to communicate technical concepts simply and successfully, to both technical and non-technical audiences.
+// Maintains relationships with other disciplines and stakeholders.
+// You’ll exhibit best behavior and exemplify our company behaviors.
+// Experience writing distributed, highly scalable, and available backend systems and APIs.
+
+// Manage, identify, support, and provide opportunities for Software Engineers and Quality Analysts/Engineers to be successful in their roles in addition to achieving their career development goals.
+// Work with a team of Software Engineers and Quality Analysts/Engineers to deliver a technical vision with an eye for customer satisfaction and security.
+// Experienced in leading engineering teams with focus across Web, Mobile, and/or connected devices (preferably in React, React Native, Native, and Node.JS technology stacks)
+// Experienced in working with stakeholders to provide clear data driven status updates, communicate blockers and remediations, and building deliverable rollout plans with timelines, risks, and resource needs through-out the project lifecycle.
+// Experienced in clarting with Product Managers and Product Owners to influence roadmap and backlog prioritization.
+// Proactively identify technology gaps and opportunities while mitigating risks created through complexity (technical and regulatory).
+// Measure owns and drive predictability through Operational Excellence and transparency through data and KPIs.
+// Own your software by helping to guide your team to deliver it while providing appropriate support for end-users.
+// Passion for setting and promoting engineering best practices and standards.
+// Experienced in tackle projects that solve open-ended, ambiguous problems that are often beyond the scope of an immediate team.
+// Influencing outcomes across multiple teams and with cross-functional partners, often requiring conflict resolution and managing ambiguity.
